9d7113dfd6b835b34a388e3c8cef01a4de520218
Stuart Ellis <stuart@ioppublishing.org>
2019-09-12 16:51:57 +0100

    Remove Git config

--- do.sh
+++ do.sh
@@ -9,7 +9,6 @@ CONTAINER_DIR="${PWD}"
 CONTAINER_NAME="$(basename "${CONTAINER_DIR}")"
 
 AWS_CREDENTIALS_DIR="${HOME}/.aws"
-GIT_CONFIG_FILE="${HOME}/.gitconfig"
 SSH_CREDENTIALS_DIR="${HOME}/.ssh"
 
 BUILDER_DOCKERFILE_PATH="${CONTAINER_DIR}"
@@ -19,17 +18,16 @@ BUILDER_IMAGE_ID="${CONTAINER_NAME}-builder:${BUILDER_IMAGE_VERSION}"
 case $1 in
   info)
     echo "Container      | Container name        | ${CONTAINER_NAME}"
-    echo "Docker builder | Current image ID      | ${BUILDER_IMAGE_ID}"
-    echo "Docker builder | Current image version | ${BUILDER_IMAGE_VERSION}"
+    echo "Docker         | Current image ID      | ${BUILDER_IMAGE_ID}"
+    echo "Docker         | Current image version | ${BUILDER_IMAGE_VERSION}"
     echo "AWS            | Credentials directory | ${AWS_CREDENTIALS_DIR}"
-    echo "Git            | Configuration file    | ${GIT_CONFIG_FILE}"
     echo "SSH            | Credentials directory | ${SSH_CREDENTIALS_DIR}"
     ;;
   setup)
     docker build "${BUILDER_DOCKERFILE_PATH}/" -t "${BUILDER_IMAGE_ID}"
     ;;
   shell)
-    docker run --rm -it --mount "type=bind,source=${AWS_CREDENTIALS_DIR},destination=/root/.aws,readonly" --mount "type=bind,source=${GIT_CONFIG_FILE},destination=/root/.gitconfig,readonly" --mount "type=bind,source=${SSH_CREDENTIALS_DIR},destination=/root/.ssh,readonly" "${BUILDER_IMAGE_ID}" /bin/bash
+    docker run --rm -it --mount "type=bind,source=${AWS_CREDENTIALS_DIR},destination=/root/.aws,readonly" --mount "type=bind,source=${SSH_CREDENTIALS_DIR},destination=/root/.ssh,readonly" "${BUILDER_IMAGE_ID}" /bin/bash
     ;;
   *)
     echo "$1 is not a valid command"
