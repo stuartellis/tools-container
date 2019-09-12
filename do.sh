a1b733fa0fc14d44444fdc74b900ff93f6db6561
Stuart Ellis <stuart@ioppublishing.org>
2019-09-12 10:27:01 +0100

    Do not mount projects directory

--- do.sh
+++ do.sh
@@ -8,7 +8,6 @@ set -eu
 CONTAINER_DIR="${PWD}"
 CONTAINER_NAME="$(basename "${CONTAINER_DIR}")"
 
-PROJECTS_DIR="${HOME}/code"
 AWS_CREDENTIALS_DIR="${HOME}/.aws"
 GIT_CONFIG_FILE="${HOME}/.gitconfig"
 SSH_CREDENTIALS_DIR="${HOME}/.ssh"
@@ -20,7 +19,6 @@ BUILDER_IMAGE_ID="${CONTAINER_NAME}-builder:${BUILDER_IMAGE_VERSION}"
 case $1 in
   info)
     echo "Container      | Container name        | ${CONTAINER_NAME}"
-    echo "Project        | Projects directory    | ${PROJECTS_DIR}"
     echo "Docker builder | Current image ID      | ${BUILDER_IMAGE_ID}"
     echo "Docker builder | Current image version | ${BUILDER_IMAGE_VERSION}"
     echo "AWS            | Credentials directory | ${AWS_CREDENTIALS_DIR}"
@@ -31,7 +29,7 @@ case $1 in
     docker build "${BUILDER_DOCKERFILE_PATH}/" -t "${BUILDER_IMAGE_ID}"
     ;;
   shell)
-    docker run --rm -it --mount "type=bind,source=${PROJECTS_DIR},destination=/projects" --mount "type=bind,source=${AWS_CREDENTIALS_DIR},destination=/root/.aws,readonly" --mount "type=bind,source=${GIT_CONFIG_FILE},destination=/root/.gitconfig,readonly" --mount "type=bind,source=${SSH_CREDENTIALS_DIR},destination=/root/.ssh,readonly" "${BUILDER_IMAGE_ID}" /bin/bash
+    docker run --rm -it --mount "type=bind,source=${AWS_CREDENTIALS_DIR},destination=/root/.aws,readonly" --mount "type=bind,source=${GIT_CONFIG_FILE},destination=/root/.gitconfig,readonly" --mount "type=bind,source=${SSH_CREDENTIALS_DIR},destination=/root/.ssh,readonly" "${BUILDER_IMAGE_ID}" /bin/bash
     ;;
   *)
     echo "$1 is not a valid command"
